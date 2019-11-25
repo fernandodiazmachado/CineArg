eceeef47bd72598a100632b2c0ded36124ed69e6
nestorbongiorno <49076817+nestorbongiorno@users.noreply.github.com>
2019-11-25 20:40:08 -0300

    Update Conexion.cs

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -22,7 +22,7 @@ namespace Datos
         public Conexion()
         {
             connect = new MySqlConnection();
-            connection = "server=127.0.0.1; port=3306; database=cinearg; user id=root; password=12345";
+            connection = "server=127.0.0.1; port=3306; database=cinearg; user id=root; password=1234";
         }
 
         private MySqlConnection ConnectToBBDD()
